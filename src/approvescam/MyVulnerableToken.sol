4678e2ac84afeff78155a98845d2ecfba2986e28
ordili <qinqiqili@163.com>
2025-04-03 19:40:29 +0800

    add approve scam attacker demo

---
+++ src/approvescam/MyVulnerableToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract MyVulnerableToken is ERC20 {
+    constructor() ERC20("MyVulnerableToken", "MTK")
+    Ownable(msg.sender)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // 没有检查是否是Owner才能发起approve，容易受到 approve scam 攻击
+    function approve(address spender, uint256 value) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, value);
+        return true;
+    }
+}
