90cc0db77511378a52c3b8244920d07ed70f2952
ordili <qinqiqili@163.com>
2025-04-03 21:56:38 +0800

    add commonts

--- src/attack/lenderattack/v2/EthLenderPool.sol
+++ src/attack/lenderattack/v2/EthLenderPool.sol
@@ -11,6 +11,7 @@ contract EthLenderPool {
     mapping(address => uint256) public balances;
 
     error RepayFailed();
+
     event Deposit(address indexed who, uint256 amount);
     event Withdraw(address indexed who, uint256 amount);
 
@@ -28,6 +29,12 @@ contract EthLenderPool {
         SafeTransferLib.safeTransferETH(msg.sender, amount);
     }
 
+    /**
+    *风险点：只判断了还款后 EthLenderPool 合约余额的大小，但这个余额可能包含用户存在合约中的资产，
+    *对于这部分资产，用户是可以随时提走的。如下是攻击者合约，这里使用了 attack 函数发起闪电贷，
+    *并顺利提走了资金，它是怎么做到的？在回调函数 execute 中，攻击者将闪电贷借来的资金又存到了资金池中，
+    *从而保证资金池中的资金并没有减少，但现在资金却被记录成了攻击者的！
+    */
     function flashLoan(uint256 amount) external {
         uint256 balanceBefore = address(this).balance;
         IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
