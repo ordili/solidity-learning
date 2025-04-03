d96fb08ff1c2b69fdb4c7e67eb9f5ffb9d7299d0
ordili <qinqiqili@163.com>
2025-04-03 21:33:50 +0800

    change import path

--- src/attack/approvescam/MyVulnerableToken.sol
+++ src/attack/approvescam/MyVulnerableToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "../../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract MyVulnerableToken is ERC20, Ownable {
     constructor() ERC20("MyVulnerableToken", "MTK")
--- src/attack/lenderattack/LenderPool.sol
+++ src/attack/lenderattack/LenderPool.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-//import "@openzeppelin/utils/Address.sol";
-//import "@openzeppelin/security/ReentrancyGuard.sol";
-//import "@openzeppelin/token/ERC20/ERC20.sol";
-//import "@openzeppelin/access/Ownable.sol";
-import "../../../lib/openzeppelin-contracts/contracts/utils/Address.sol";
+import "openzeppelin-contracts/contracts/utils/Address.sol";
 import "./MyToken.sol";
 import "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
