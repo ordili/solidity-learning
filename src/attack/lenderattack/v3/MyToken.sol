0b1f511920fca8aaa51c0e26e030cc34684fa022
ordili <qinqiqili@163.com>
2025-04-03 22:05:27 +0800

    add MyToken.sol

---
+++ src/attack/lenderattack/v3/MyToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MTK", "MT") Ownable(msg.sender) {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
