7eb176dd1e66fb1b9aa4197b60614e508536fe7d
ordili <qinqiqili@163.com>
2025-04-04 15:41:57 +0800

    Add comment.

--- src/attack/dosattack/Attack.sol
+++ src/attack/dosattack/Attack.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.10;
 
 /**
-*该合约没有定义 receive 或 fallback 函数来接收 ETH，因此所有给该合约发送 ETH
-*的交易将会失败。或者定义一个恶意的 fallback 函数，通过 assert(false)
-*让所有的转账交易失败。
-*/
+ *   该合约没有定义 receive 或 fallback 函数来接收 ETH，因此所有给该合约发送 ETH
+ *   的交易将会失败。或者定义一个恶意的 fallback 函数，通过 assert(false)
+ *   让所有的转账交易失败。
+ * @title
+ * @author Gidong
+ * @dev
+ */
 contract Attack {
     KingOfEther kingOfEther;
 
@@ -17,6 +20,8 @@ contract Attack {
     //     assert(false);
     // }
 
+    /// @notice
+    /// @param
     function attack() public payable {
         kingOfEther.claimThrone{value: msg.value}();
     }
