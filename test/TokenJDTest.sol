d365943ceb2dca79de2887ee3ac0f5a4b66f6894
ordili <qinqiqili@163.com>
2025-09-02 21:33:56 +0800

    modiofy TokenJDTest

--- test/TokenJDTest.sol
+++ test/TokenJDTest.sol
@@ -6,14 +6,26 @@ import {Test} from "forge-std/Test.sol";
 import {Token} from "../src/vulnerable/Token.sol";
 
 contract TokenJDTest is Test {
-    TokenJD public tokenJD;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    TokenJD public tokenJD;
+    address public sender;
     constructor() {
+        sender = makeAddr("user");
+        vm.deal(sender, 10 ether);
+        vm.prank(sender);
         tokenJD = new TokenJD();
+
     }
 
-    function testTransfer() public {
+    function testTransferEvent() public {
         address other = makeAddr("other");
+        // 期望下一个emit的事件是 Transfer，并且参数分别是 address(this), alice, 100
+        vm.expectEmit(true, true, true, true);
+        // 定义期望的事件：必须是完全相同的签名和参数
+        emit Transfer(sender, other, 10);
+        vm.prank(sender);
         tokenJD.transfer(other, 10 wei);
         assertEq(10 wei, tokenJD.balanceOf(other));
     }
