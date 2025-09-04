f7963b152c2fc22d32ff8a63481e369b82c9a3e0
ordili <qinqiqili@163.com>
2025-09-04 17:11:07 +0800

    modify import

--- src/ethernaut/Dex.sol
+++ src/ethernaut/Dex.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token1 is ERC20("token1", "t1") {
     constructor() {
