1e9bad6b90eb3dfca36c9c32ae6da04dbb017218
ordili <qinqiqili@163.com>
2025-09-24 11:24:55 +0800

    Modify DemoTest.sol

--- test/DemoTest.sol
+++ test/DemoTest.sol
@@ -5,8 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 
 contract DemoTest is Test {
     function testXX() public pure {
-        address account = 0xC2929457BDb86Bca5759ABBB64D0e6264367aF6a;
-        bytes memory sig = abi.encodeWithSignature("balanceOf(address)", account);
-        console.logBytes(sig);
+        console.log("max is :", type(uint).max);
+        console.log("min is :", type(uint).min);
     }
 }
